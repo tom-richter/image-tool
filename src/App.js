0ae6433d5fda714f7704020cfb98be5e79cbf95e
Tom Richter <tomrichter99@gmail.com>
2021-11-05 19:23:50 +0100

    add image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/react-image-editor';
 
+const myTheme = {
+  // Theme object to extends default dark theme.
+};
 function App() {
   return (
     <div className="App">
@@ -9,6 +14,29 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ImageEditor
+          includeUI={{
+            loadImage: {
+              path: 'img/sampleImage.jpg',
+              name: 'SampleImage',
+            },
+            theme: myTheme,
+            menu: ['shape', 'filter'],
+            initMenu: 'filter',
+            uiSize: {
+              width: '1000px',
+              height: '700px',
+            },
+            menuBarPosition: 'bottom',
+          }}
+          cssMaxHeight={500}
+          cssMaxWidth={700}
+          selectionStyle={{
+            cornerSize: 20,
+            rotatingPointOffset: 70,
+          }}
+          usageStatistics={true}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
